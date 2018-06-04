e705bd86b968298e827e70845a5f5704c143e3e4
duantuigegelxh <38995556+duantuigegelxh@users.noreply.github.com>
2018-06-04 16:26:26 +0800

    Update boot_crm.sql

--- boot_crm.sql
+++ boot_crm.sql
@@ -1,17 +1,4 @@
-/*
-SQLyog Ultimate v8.32 
-MySQL - 5.5.40 : Database - boot_crm
-*********************************************************************
-*/
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`boot_crm` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `boot_crm`;
